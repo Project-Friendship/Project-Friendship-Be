5598b40a2e19c372d6335dd36d4c0ae307201daf
David Schoenbauer <d.schoenbauer@ctimeetingtech.com>
2022-08-30 22:03:03 -0500

    disable get password

--- mcalib.js
+++ mcalib.js
@@ -22,30 +22,30 @@ function setErrorPrefix(string) {
 // 1 or 2 arguments:  string name of database host, optional .pgpass file path
 //    default filepath is ~/.pgpass
 
-function getPGPassword(dbHost, filepath) {
-    try {
-	var pgpassFile = filepath;
-	if (filepath == undefined)
-	    pgpassFile = os.homedir() + "\\" + path.sep + ".pgpass";
-	var pgpassArr = fs.readFileSync(pgpassFile, 'utf8').split("\n");
-    } catch (err) {
-	throw new Error(`${ePrefix}Could not open ${pgpassFile}\n${err}`);
-    }
+// function getPGPassword(dbHost, filepath) {
+//     try {
+// 	var pgpassFile = filepath;
+// 	if (filepath == undefined)
+// 	    pgpassFile = os.homedir() + "\\" + path.sep + ".pgpass";
+// 	var pgpassArr = fs.readFileSync(pgpassFile, 'utf8').split("\n");
+//     } catch (err) {
+// 		throw new Error(`${ePrefix}Could not open ${pgpassFile}\n${err}`);
+//     }
     
-    const dbHostRE = new RegExp("^" + dbHost + ":.*");
-    // dbHostRE matches first line that begins with dbHost
+//     const dbHostRE = new RegExp("^" + dbHost + ":.*");
+//     // dbHostRE matches first line that begins with dbHost
 
-    for (var i=0;  i < pgpassArr.length;  i++)
-	if (pgpassArr[i].match(dbHostRE))
-	    break;
-    if (i == pgpassArr.length) {
-	throw new Error(
-	    `\n${ePrefix}Could not find a line for ${dbHost} in ${pgpassFile}`);
-    }
+//     for (var i=0;  i < pgpassArr.length;  i++)
+// 	if (pgpassArr[i].match(dbHostRE))
+// 	    break;
+//     if (i == pgpassArr.length) {
+// 	throw new Error(
+// 	    `\n${ePrefix}Could not find a line for ${dbHost} in ${pgpassFile}`);
+//     }
     
-    return pgpassArr[i].replace(/^.*:/, "");
+//     return pgpassArr[i].replace(/^.*:/, "");
     
-}
+// }
 
 // returns array of IP addresses for this computer 
 
@@ -69,4 +69,4 @@ function getIPAddresses() {
     return addresses;
 }
 
-module.exports = { setErrorPrefix, getPGPassword, getIPAddresses };
+module.exports = { setErrorPrefix, /*getPGPassword,*/ getIPAddresses };
