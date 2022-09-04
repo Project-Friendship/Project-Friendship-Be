6a7eb74ed9642642ec3eb71d16e1e2330e3bbb2c
David Schoenbauer <d.schoenbauer@ctimeetingtech.com>
2022-09-04 13:52:22 +0000

    update output

--- routes/Root.js
+++ routes/Root.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (request, response) => {
-	    response.sendStatus(200)
+	    response.send("App is running")
 })
